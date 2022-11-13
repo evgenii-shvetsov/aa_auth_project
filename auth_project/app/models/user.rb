4f517b124ecd9d2374947fb7b43eac6f79c1d641
evgenii-shvetsov <sea.dev91@gmail.com>
2022-11-13 15:25:05 -0800

    Finish phase 2

--- auth_project/app/models/user.rb
+++ auth_project/app/models/user.rb
@@ -1,2 +1,24 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :bigint           not null, primary key
+#  username        :string           not null
+#  password_digest :string           not null
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
 class User < ApplicationRecord
+    
+
+    validates :username, presence: true, uniqueness: true
+    validates :password_digest, presence: true
+
+    def password=(password) #setter method
+        self.password_digest = BCrypt::Password.create(password)
+    end
+
+    def is_password?(password)
+        BCrypt::Password.new(password_digest).is_password?(password)
+    end
 end
